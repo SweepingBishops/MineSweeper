82198bc602c8359d3c0734eebca7560e06c7a6ec
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-01-10 22:03:52 +0530

    Rewritten plantMines.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from tkinter import *
 import random
 import time
-from plantMines import *
+from plantMines_rewrite import *
 
-def onClick(clickPosition):
-	#print(clickPosition)
-	if clickPosition not in minePositions:	#if clicked square does not contain mine it is coloured green
-		Label(main,text= squareValues[clickPosition[0] , clickPosition[1]],bg='green').grid(row=clickPosition[0],column=clickPosition[1])
+###Global variables###
+firstClickFlag = True
+minePositions , squareValues = {}, {}
+
+def onClick(i,j):
+	global firstClickFlag
+	global minePositions
+	global squareValues
+
+	if firstClickFlag:
+		minePositions, squareValues = plantMines(i,j,gridSize,mineCount)
+		print(minePositions)
+		firstClickFlag = False
+
+	if (i,j) not in minePositions:	#if clicked square does not contain mine it is coloured green
+		Label(main,text= squareValues[(i,j)],bg='green').grid(row=i,column=j)
 		#open_squares
 	else:	#if the square contains a mine it is coloured red and the game exits
-		Label(main,text= str(clickPosition[0]) +','+ str(clickPosition[1]),bg='red').grid(row=clickPosition[0],column=clickPosition[1])
+		Label(main,text= str(i) +','+ str(j),bg='red').grid(row=i,column=j)
 		print('You lost:(')
 		#high scores
 		main.after(2000,func=exit)
@@ -18,7 +30,6 @@ def onClick(clickPosition):
 
 gridSize = int(input('Enter grid size: '))
 mineCount = int(input('Enter number of mines: '))
-minePositions , squareValues = plantMines(gridSize,mineCount)
 
 #creating main screen
 main = Tk(className='Minesweeper')
@@ -28,8 +39,7 @@ main = Tk(className='Minesweeper')
 
 for i in range(gridSize):
 	for j in range(gridSize):
-		Button(main,text=str(i)+','+str(j),command=lambda i=i , j=j: onClick((i,j))).grid(row=i,column=j)
+		Button(main,text=str(i)+','+str(j),command=lambda i=i , j=j: onClick(i,j)).grid(row=i,column=j)
 		#i is the row number and j is the column number
-print(minePositions)
 #print(squareValues)
 mainloop()
