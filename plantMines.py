b82b362e4513e01e55a5f70a289ec49c17942464
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-01-14 16:41:16 +0530

    Update plantMines.py

--- plantMines.py
+++ plantMines.py
@@ -17,9 +17,9 @@ def plantMines(i,j,gridSize,mineCount):		#function to plant the mines and determ
 	minePositions = set()	#initiated as a set to prevent duplicates
 	squareValues = {}
 	while len(minePositions) < mineCount:
-		x,y = randint(0,gridSize - 1),randint(0,gridSize - 1)
-		if (x,y) != (i,j) and (x,y) not in identifyNeighbouringSquares(i, j, gridSize):
-			minePositions.add( (x,y) )	#uses random for deciding where to put mines
+		x,y = randint(0,gridSize - 1),randint(0,gridSize - 1)	#uses random for deciding where to put mines
+		if (x,y) != (i,j) and (x,y) not in identifyNeighbouringSquares(i, j, gridSize):	   #the square first clicked and its neighbours don't have mines
+			minePositions.add( (x,y) )	
 
 	for i in range(gridSize):
 		for j in range(gridSize):
@@ -30,7 +30,8 @@ def plantMines(i,j,gridSize,mineCount):		#function to plant the mines and determ
 		for neighbouringSquare in identifyNeighbouringSquares(i,j,gridSize):
 			if neighbouringSquare not in minePositions:
 				squareValues[neighbouringSquare] += 1
+				
 	for key in squareValues:
 		if squareValues[key] == 0:
-			squareValues[key] = None
+			squareValues[key] = None	#turns all zeroes into Nones for aesthetic reasons when displayed to the user
 	return minePositions , squareValues
