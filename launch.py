f800838bd6ae61587b61955a293532754b7a6925
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-01-22 22:01:06 +0530

    Update

--- launch.py
+++ launch.py
@@ -5,55 +5,61 @@ from functools import partial
 from time import time
 from PIL import Image, ImageTk
 
-###Global variables###
-firstClickFlag = True
-bIdentity , minePositions , squareValues = {}, {}, {}
-clickedSquares = 0
-startTime, endTime = 0,0
 
 def main():
+	###Global variables###
+	firstClickFlag,gameover = True, False
+	bIdentity , minePositions , squareValues = {}, {}, {}
+	startTime, endTime, clickedSquares = 0,0,0
+
 	def leftClick(i,j):
-		global firstClickFlag, minePositions, squareValues, clickedSquares, startTime, endTime
+		nonlocal gameover, firstClickFlag, clickedSquares ,minePositions, squareValues, startTime, endTime
 
 		button = bIdentity[(i,j)]	#stores the object at (i,j) into local variable button
-		if button['image'] != '':	#if the square is flagged then it cannot be clicked
+		if button['image'] or gameover:	#button press function is not executed if the game is over or the button is flagged
 			return
 		button['state'] = 'disabled'	#so that the same button can't be clicked multiple times
 
 		if firstClickFlag:
 			minePositions, squareValues = plantMines(i,j,gridSize,mineCount)	#the mines are planted at the first click
 			firstClickFlag = False
-			startTime = time()
+			startTime = time()	#stores the time when the user starts playing
 
 		if (i,j) not in minePositions:	#if clicked square does not contain mine it is coloured green
 			button.configure(text = squareValues[(i,j)], bg = 'green',activebackground = 'green')
+			clickedSquares += 1	
 		else:				#if the square contains a mine it is coloured red
+			for mine in minePositions:	#all the other mines are coloured orange
+					bIdentity[mine].configure(height=46, width=62, bg='orange', activebackground='orange', image=mineImage)
 			button.configure(height=46, width=62, bg = 'red', activebackground = 'red', image=mineImage)
-			for button in bIdentity.values():     #All buttons are diabled once a mine is clicked and the rest of the mines are shown 
-				button['state'] = 'disabled'
-				for mine in minePositions:
-					if mine != (i,j):
-						bIdentity[mine].configure(height=46, width=62, bg='orange', activebackground='orange', image=mineImage)
+			gameover = True
 			print('You lost:(')
+			return
 
-		clickedSquares += 1	
+		if clickedSquares == (gridSize**2 - mineCount):		#checks if the game is won
+			endTime = time()	#stores time when the game is won
+			print('You won!')
+			gameover = True
+			for mine in minePositions:	#All mines are exposed.
+				bIdentity[mine].configure(height=46,width=62,image = flagImage)
+
+			score = round(endTime - startTime)
+			hours = score//3600
+			score %= 3600
+			minutes = score//60
+			seconds = score%60
+
+			print(f'Time taken:{hours}h {minutes}min {seconds}s')
+			return
 
 		if squareValues[(i,j)] == None:		#opens neighbouring squares if current squareValue is 0 (game rule)
 			neighbouringSquares = identifyNeighbouringSquares(i, j, gridSize)
 			for neighbouringSquare in neighbouringSquares:
 				bIdentity[neighbouringSquare].invoke()
 
-		if clickedSquares == (gridSize**2 - mineCount):
-			print('You won!')
-			endTime = time()
-			for button in bIdentity.values():	#All buttons are disabled once the game is won.
-				button['state'] = 'disabled'
-				for mine in minePositions:	#All mines are exposed.
-					bIdentity[mine].configure(bg='blue',activebackground='blue')
-			print(f'Time taken:{round(endTime - startTime,2)}s')
-
-
 	def rightClick(event):
+		if gameover: 
+			return
 		button = event.widget
 
 		if button['image'] == '' and button['bg'] == '#d9d9d9':		#Flags the square if it isn't and is not already leftClicked.
@@ -66,15 +72,17 @@ def main():
 			try:
 				gridSize = int(input('Enter grid size: '))
 				mineCount = int(input('Enter number of mines: '))
-				if mineCount > gridSize**2 - 10:
-					raise Exception
-				break
+				if gridSize < 4:
+					raise Exception('Grid size must at least 4.\n')
+				elif mineCount > gridSize**2 - 10 or mineCount < 1:
+					raise Exception(f'For grid size = {gridSize} the number of mines should be between 1 and {max(gridSize**2 - 10,1)}.\n')
+				break                    
 			except ValueError:
 				gridSize = None
 				print('Invalid Input\n')
 
-			except Exception:
-				print(f'For grid size = {gridSize} the number of mines should be between 1 and {gridSize**2 - 10}.\n')
+			except Exception as error:
+				print(error.args[0])
 				gridSize = None
 	#creating main window
 	mainWindow = tkinter.Tk(className='Minesweeper')
