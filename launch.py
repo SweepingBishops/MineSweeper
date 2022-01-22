461dbb03027f889f3ff1c4758246a031b974d96e
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-01-22 10:50:24 +0530

    Input Error handling added.

--- launch.py
+++ launch.py
@@ -7,10 +7,12 @@ from functools import partial
 ###Global variables###
 firstClickFlag = True
 bIdentity , minePositions , squareValues = {}, {}, {}
+clickedSquares = 0
+#gridSize , mineCount = None, None
 
 def main():
 	def onClick(i,j):
-		global firstClickFlag, minePositions, squareValues
+		global firstClickFlag, minePositions, squareValues, clickedSquares#, gridSize, mineCount
 
 		button = bIdentity[(i,j)]	#stores the object at (i,j) into local variable button
 		button['state'] = 'disabled'	#so that the same button can't be clicked multiple times
@@ -22,19 +24,36 @@ def main():
 		if (i,j) not in minePositions:	#if clicked square does not contain mine it is coloured green
 			tkinter.Label(mainWindow,text= squareValues[(i,j)],bg='green', height = 2, width = 4).grid(row=i,column=j)
 		else:				#if the square contains a mine it is coloured red and the game exits
-			tkinter.Label(mainWindow,text= str(i) +','+ str(j),bg='red', height = 2, width = 4).grid(row=i,column=j)
+			tkinter.Label(mainWindow,bg='red', height = 2, width = 4).grid(row=i,column=j)
 			print('You lost:(')
 			mainWindow.after(2000,func=exit)
-			
+
+		clickedSquares += 1	
+
 		if squareValues[(i,j)] == None:		#opens neighbouring squares if current squareValue is 0 (game rule)
 			neighbouringSquares = identifyNeighbouringSquares(i, j, gridSize)
 			for neighbouringSquare in neighbouringSquares:
 				bIdentity[neighbouringSquare].invoke()
+		if clickedSquares == (gridSize**2 - mineCount):
+			print('You won!')
+			mainWindow.after(2000,func=exit)
 
 
-	gridSize = int(input('Enter grid size: '))
-	mineCount = int(input('Enter number of mines: '))
+	gridSize = None
+	while gridSize == None:
+			try:
+				gridSize = int(input('Enter grid size: '))
+				mineCount = int(input('Enter number of mines: '))
+				if mineCount > gridSize**2 - 10:
+					raise Exception
+				break
+			except ValueError:
+				gridSize = None
+				print('Invalid Input\n')
 
+			except Exception:
+				print(f'For grid size = {gridSize} the number of mines should be between 1 and {gridSize**2 - 10}.\n')
+				gridSize = None
 	#creating main screen
 	mainWindow = tkinter.Tk(className='Minesweeper')
 	mainWindow.option_add('*Font','22')
@@ -42,7 +61,7 @@ def main():
 	for i in range(gridSize):
 		for j in range(gridSize):
 			#creates and sets the buttons onto the window
-			button = tkinter.Button(mainWindow, text= f'{str(i)},{str(j)}', command = partial(onClick,i,j) , height = 2, width = 4)
+			button = tkinter.Button(mainWindow, text= '', command = partial(onClick,i,j) , height = 2, width = 4)
 			button.grid(row=i,column=j)
 			
 			bIdentity[(i,j)] = button	#adds the object into the dictionary so that it can be used later
