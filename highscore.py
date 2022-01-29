b5338995042bd63a02d8a2ccd166b1e896d2def9
pantheragenius <98640795+pantheragenius@users.noreply.github.com>
2022-01-29 17:17:36 +0530

    Update highscore.py

--- highscore.py
+++ highscore.py
@@ -1,26 +1,72 @@
-import mysql.connector as mc
 import tabulate
 import tkinter as tk
-conn=mc.connect(host='localhost',user='root',database='minesweeper',passwd='')
+import menu1
+
+import mysql.connector as mc
+conn=mc.connect(host='localhost',user='root',database='minesweeper',passwd='rachu@004')
 cur=conn.cursor()
-cur.execute('select * from gridsize6 order by time;')
-b=list(cur)
-d=[]
-try:
-	for i in range(5):
-	    c=list(b[i])
-	    c.insert(0,i+1)
-	    d+=[c]
-except IndexError:
-	pass
-    
-a=tabulate.tabulate(d)
 
 #tkinter
 root=tk.Tk()
 root.title('HIGHSCORE')
-root.rowconfigure([0],minsize=150)
-root.columnconfigure([0,1,2],minsize=70)
-label=tk.Label(text=a)
-label.grid(row=0,column=1)
+
+#frames
+frame1=tk.Frame()
+frame1.pack()
+frame1.rowconfigure([0,2],minsize=40)
+frame1.rowconfigure([1],minsize=150)
+frame1.columnconfigure([0,1,2],minsize=160)
+
+frame2=tk.Frame()
+frame2.pack()
+
+#definition
+def back():
+    root.destroy()
+    menu1.main2()
+
+
+#gridsize6
+cur.execute('select * from gridsize6 order by time;')
+b=list(cur)
+d=[]
+for i in range(5):
+    c=list(b[i])
+    c.insert(0,i+1)
+    d+=[c]
+head=['Rank','Name','Time']    
+a=tabulate.tabulate(d,headers=head)
+label=tk.Label(master=frame1,text=a)
+label.grid(row=1,column=0)
+
+#gridsize8
+cur.execute('select * from gridsize8 order by time;')
+b=list(cur)
+d=[]
+for i in range(5):
+    c=list(b[i])
+    c.insert(0,i+1)
+    d+=[c]
+head=['Rank','Name','Time']    
+a=tabulate.tabulate(d,headers=head)
+label=tk.Label(master=frame1,text=a)
+label.grid(row=1,column=1)
+#gridsize10
+cur.execute('select * from gridsize10 order by time;')
+b=list(cur)
+d=[]
+for i in range(5):
+    c=list(b[i])
+    c.insert(0,i+1)
+    d+=[c]
+head=['Rank','  Name  ','Time']    
+a=tabulate.tabulate(d,headers=head)
+label=tk.Label(master=frame1,text=a)
+label.grid(row=1,column=2)
+
+#buttons
+button=tk.Button(master=frame2,text='Back',height=2,width=10,command=back)
+button.pack()
+
+
 
