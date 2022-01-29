28f7471f327bd748461247f740b2bca99f1a3e09
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-01-29 15:59:06 +0530

    Added first menu highscore

--- highscore.py
+++ highscore.py
@@ -1,15 +1,18 @@
 import mysql.connector as mc
 import tabulate
 import tkinter as tk
-conn=mc.connect(host='localhost',user='root',database='minesweeper',passwd='rachu@004')
+conn=mc.connect(host='localhost',user='root',database='minesweeper',passwd='')
 cur=conn.cursor()
-cur.execute('select * from gridsize4 order by time;')
+cur.execute('select * from gridsize6 order by time;')
 b=list(cur)
 d=[]
-for i in range(5):
-    c=list(b[i])
-    c.insert(0,i+1)
-    d+=[c]
+try:
+	for i in range(5):
+	    c=list(b[i])
+	    c.insert(0,i+1)
+	    d+=[c]
+except IndexError:
+	pass
     
 a=tabulate.tabulate(d)
 
